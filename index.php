ae5bd1a842f833dd480ae33f97a685c8654adb18
qqKazakov <leap74rus@gmail.com>
2023-08-21 07:06:37 +0300

    add: login

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<?php
+require_once __DIR__ . '/src/helpers.php';
+?>
 <!DOCTYPE html>
-<html lang="ru" data-theme="light">
+<html lang="ru" data-theme="dark">
 
 <head>
     <meta charset="UTF-8">
@@ -10,15 +13,20 @@
 
 <body>
 
-    <form class="card">
+    <form class="card" action="src/actions/login.php" method="post">
         <h2>Вход</h2>
 
-        <!-- <div class="notice error">Какая-то ошибка</div>
-    <div class="notice success">Какое-то сообщение</div> -->
+        <?php if (hasMessage('error')) : ?>
+            <div class="notice error"><?php echo getMessage('error') ?></div>
+        <?php endif; ?>
+        <!-- <div class=" notice success">Какое-то сообщение</div> -->
 
         <label for="name">
             Имя
-            <input type="text" id="name" name="name" placeholder="Иванов Иван" aria-invalid="true" required>
+            <input type="text" id="name" name="email" placeholder="ivan@areaweb.su" aria-invalid="true" required value="<?php echo old('email') ?>" <?php echo validationErrorAttr('email'); ?>>
+            <?php if (hasValidationError('email')) : ?>
+                <small><?php echo validationErrorMessage('email'); ?></small>
+            <?php endif; ?>
         </label>
 
         <label for="password">
@@ -32,6 +40,4 @@
     <p>У меня еще нет <a href="/register.html">аккаунта</a></p>
 
     <script src="assets/app.js"></script>
-</body>
-
-</html>
+    </bo
--- src/helpers.php
+++ src/helpers.php
@@ -8,70 +8,96 @@ require_once __DIR__ . '/privat.php';
 
 function redirect(string $path)
 {
-    header("Location: $path");
-    die();
+  header("Location: $path");
+  die();
 }
 
 // Валидация
 function setValidationError(string $fieldName, string $message): void
 {
-    $_SESSION['validation'][$fieldName] = $message;
+  $_SESSION['validation'][$fieldName] = $message;
 }
 
 function hasValidationError(string $fieldName): bool
 {
-    return isset($_SESSION['validation'][$fieldName]);
+  return isset($_SESSION['validation'][$fieldName]);
 }
 
 function validationErrorAttr(string $fieldName): string
 {
-    return isset($_SESSION['validation'][$fieldName]) ? 'aria-invalid="true"' : '';
+  return isset($_SESSION['validation'][$fieldName]) ? 'aria-invalid="true"' : '';
 }
 
 function validationErrorMessage(string $fieldName): string
 {
-    $message = $_SESSION['validation'][$fieldName] ?? '';
-    unset($_SESSION['validation'][$fieldName]);
-    return $message;
+  $message = $_SESSION['validation'][$fieldName] ?? '';
+  unset($_SESSION['validation'][$fieldName]);
+  return $message;
 }
 
 function setOldValue(string $key, mixed $value): void
 {
-    $_SESSION['old'][$key] = $value;
+  $_SESSION['old'][$key] = $value;
 }
 
 function old(string $key)
 {
-    $value = $_SESSION['old'][$key] ?? '';
-    unset($_SESSION['old'][$key]);
-    return $value;
+  $value = $_SESSION['old'][$key] ?? '';
+  unset($_SESSION['old'][$key]);
+  return $value;
 }
 
 // работа с файлами
 function uploadFile(array $file, string $prefix = ''): string
 {
-    $uploadFile = __DIR__ . '/../uploads';
+  $uploadFile = __DIR__ . '/../uploads';
 
-    if (!is_dir($uploadFile)) {
-        mkdir($uploadFile, 0777, true);
-    }
+  if (!is_dir($uploadFile)) {
+    mkdir($uploadFile, 0777, true);
+  }
 
-    $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
-    $fileName = $prefix . time() . ".$ext";
+  $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+  $fileName = $prefix . time() . ".$ext";
 
-    if (!move_uploaded_file($file['tmp_name'], "$uploadFile/$fileName")) {
-        die('Ошибка при загрузке файла на сервер');
-    }
+  if (!move_uploaded_file($file['tmp_name'], "$uploadFile/$fileName")) {
+    die('Ошибка при загрузке файла на сервер');
+  }
 
-    return "uploads/$fileName";
+  return "uploads/$fileName";
+}
+
+function setMessage(string $key, string $message): void
+{
+  $_SESSION['message'][$key] = $message;
+}
+
+function hasMessage(string $key): bool
+{
+  return isset($_SESSION['message'][$key]);
+}
+
+function getMessage(string $key): string
+{
+  $message = $_SESSION['message'][$key] ?? '';
+  unset($_SESSION['message'][$key]);
+  return $message;
 }
 
 // Подключение к базе
 function getPDO(): PDO
 {
-    try {
-        return new \PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';charset=utf8;dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
-    } catch (\PDOException $e) {
-        die("Connection error: {$e->getMessage()}");
-    }
+  try {
+    return new \PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';charset=utf8;dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
+  } catch (\PDOException $e) {
+    die("Connection error: {$e->getMessage()}");
+  }
+}
+
+function findUser(string $email): array|bool
+{
+  $pdo = getPDO();
+
+  $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+  $stmt->execute(['email' => $email]);
+  return $stmt->fetch(\PDO::FETCH_ASSOC);
 }
